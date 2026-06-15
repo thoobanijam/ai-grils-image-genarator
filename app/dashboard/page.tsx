84554f5fe5f47011c4b71e1b51589cb25ff47c0d
unknown <thoobabsc@gmail.com>
2026-06-15 22:48:40 +0300

    fix build issues for vercel deployment

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 // app/dashboard/page.tsx
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import FreeAi_Girl from "../Components/FreeAi_Girl";
 import ExploreCreative from "../Components/ExploreCreative";
